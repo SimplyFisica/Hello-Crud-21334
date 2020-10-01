2d5dbb1c4dcd21a20db22220ab74f1c0ec66d441
SimplyFisica <66970230+SimplyFisica@users.noreply.github.com>
2020-09-30 21:39:48 -0300

    Update note.js

--- api/routes/note.js
+++ api/routes/note.js
@@ -1,20 +1,9 @@
 const express = require('express');
+const Note    = require('../models/Note');
+
 const router = express.Router();
 
-const Note = require('../models/Note');
-
-// POST /notes
-router.post('/notes', (req, res, next) => {
-  const note = new Note({
-    title: req.body.title,
-    text: req.body.text
-  });
-  note.save((err, note) => {
-    if (err) return next(err);
-    res.status(201).json(note);
-  });
-});
-// GET /notes
+// GET /notes (todas las notas)
 router.get('/notes', (req, res, next) => {
   Note.find()                  // todos los docs de notes
     .select('_id title text')  // como SELECT en SQL
@@ -31,15 +20,16 @@ router.get('/notes', (req, res, next) => {
         }
       }));
       res.status(200).json({
-        count: notes.length,   // la cantidad de elementos en notes
+        count: notes.length,
         notes: notes,
-        create: {              // como crear una nota
+        create: {
           method: 'POST',
           url: `${req.protocol}://${req.hostname}:3000/api/notes`
         }
       });
     });
 });
+
 // GET /notes/id
 router.get('/notes/:id', (req, res, next) => {
   Note.findById(req.params.id)
@@ -62,6 +52,19 @@ router.get('/notes/:id', (req, res, next) => {
       });
     });
 });
+
+// POST /notes
+router.post('/notes', (req, res, next) => {
+  const note = new Note({
+    title: req.body.title,
+    text: req.body.text
+  });
+  note.save((err, note) => {
+    if (err) return next(err);
+    res.status(201).json(note);
+  });
+});
+
 // PUT /notes/id
 router.put('/notes/:id', (req, res, next) => {
   const note = {
@@ -79,6 +82,7 @@ router.put('/notes/:id', (req, res, next) => {
     res.status(200).json(note);
   });
 });
+
 // DELETE /notes/id
 router.delete('/notes/:id', (req, res, next) => {
   Note.findByIdAndRemove(req.params.id).exec((err, note) => {
